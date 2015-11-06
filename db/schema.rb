0f07050b608aa25977bf379f3027ca615aac1bd2
Katherine Beam <k.beam@umiami.edu>
2015-11-06 13:34:29 -0500

    Fixed login page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106033230) do
+ActiveRecord::Schema.define(version: 20151106182537) do
 
   create_table "adventures", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20151106033230) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "username"
+    t.text     "bio"
+    t.string   "location"
+    t.string   "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -37,6 +46,10 @@ ActiveRecord::Schema.define(version: 20151106033230) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "username"
+    t.text     "bio"
+    t.string   "location"
+    t.string   "picture"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
